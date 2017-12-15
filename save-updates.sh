ce6ec3b5dbe18061de144180929bf4f1d210f367
John Pham <johnphammail@gmail.com>
2017-12-15 13:27:57 -0800

    Fri Dec 15 13:27:57 PST 2017

--- save-updates.sh
+++ save-updates.sh
@@ -2,8 +2,12 @@
 
 # This script is used to save the updates for the website
 
-#npm run generate;
+npm run generate;
+echo "Staging changes";
 git add .;
+echo "Commiting the changes with the current date and time";
 message=$(date);
 git commit -m "$message";
+echo "Pushing the changes to GitHub";
 git push;
+echo "Your changes are saved and the new files are generated in /dist. To deploy live, move the contents of that folder into the vcg server";
